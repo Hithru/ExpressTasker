c719dff4762b4e3d7643f4388b450c2c3f98f437
Hithru <56365929+Hithru@users.noreply.github.com>
2021-09-17 21:04:26 +0530

    Review Bar

--- express-tasker/src/component/LandingPage/LandingPage_Reviews.js
+++ express-tasker/src/component/LandingPage/LandingPage_Reviews.js
@@ -10,49 +10,49 @@ const LandingPage_Reviews = () => {
           <img src="Images/sean.jpg" alt="" />
           <div className="review-text">
             <p>
-              Using TaskRabbit to have a new bookcase built was a great choice!
-              Rick did wonderful work with a job that was much bigger than we
-              anticipated.
+              Using ExpressTaker to have a new bookcase built was a great
+              choice! Rick did wonderful work with a job that was much bigger
+              than we anticipated.
             </p>
-            <h6>Sean P.</h6>
-            <p>Tampa, FL </p>
+            <h6>Sean Paul</h6>
+            <p>Kandy </p>
           </div>
         </div>
 
         <div className="reviews">
-          <img src="Images/hunter.jpg" alt="" />
+          <img src="Images/shamila.jpg" alt="" />
           <div className="review-text">
             <p>
               I'd been agonizing over how to get my new flat screen mounted to
-              my wall. Nick arrived the same day with all the tools for the job
-              and was just a super nice guy.
+              my wall. Indika arrived the same day with all the tools for the
+              job and was just a super nice guy.
             </p>
-            <h6>Hunter C.</h6>
-            <p>Scottsdale, AZ</p>
+            <h6>Shamila Nuwan</h6>
+            <p>Ambalangoda</p>
           </div>
         </div>
 
         <div className="reviews">
-          <img src="Images/michael.jpg" alt="" />
+          <img src="Images/Ruwan.jpg" alt="" />
           <div className="review-text">
             <p>
               I finally have expertly installed shelves and additional storage
               in my tiny house, all thanks to my Tasker.{" "}
             </p>
-            <h6>Michael K.</h6>
-            <p>Phoenix, AZ</p>
+            <h6>Ruwan Dayananda</h6>
+            <p>Colombo</p>
           </div>
         </div>
 
         <div className="reviews">
-          <img src="Images/josh.jpg" alt="" />
+          <img src="Images/Hithru.jpg" alt="" />
           <div className="review-text">
             <p>
-              TaskRabbit makes moving into your new apartment a 1 hr job instead
-              of 1 day job! Moving was seamless!
+              ExpressTaker makes moving into your new apartment a 1 hr job
+              instead of 1 day job! Moving was seamless!
             </p>
-            <h6>Josh B.</h6>
-            <p>Mesa, AZ</p>
+            <h6>Hithru De Alwis</h6>
+            <p>Nattandiya</p>
           </div>
         </div>
       </div>
